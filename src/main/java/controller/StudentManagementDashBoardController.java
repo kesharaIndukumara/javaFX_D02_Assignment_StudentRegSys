11b3d4e8223e6d1c84d0ec7f6b24ad6d505e5db8
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-05-10 15:39:25 +0530

    final

--- src/main/java/controller/StudentManagementDashBoardController.java
+++ src/main/java/controller/StudentManagementDashBoardController.java
@@ -3,13 +3,18 @@ package controller;
 import db.dbConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import lombok.Setter;
+import javafx.stage.Stage;
 import model.Student;
 
-public class StudentManagementDashBoardController {
-    @Setter
-    private Student stObj;
+import java.io.IOException;
+
+public class StudentManagementDashBoardController  {
+    private Integer index;
 
 
     @FXML
@@ -30,9 +35,25 @@ public class StudentManagementDashBoardController {
     @FXML
     private Label lblPassword;
 
-    @FXML
-    void btnOnAction(ActionEvent event) {
+    public void setLogStudentIndex(int index) {
+        this.index = index;
 
+        Student student = dbConnection.getInstance().getStudentList().get(index);
+        lblId.setText(student.getId()+"");
+        lblFName.setText(student.getFName());
+        lblEmail.setText(student.getEmail());
+        lblContactNumber.setText(student.getPhoneNumber());
+        lblAddress.setText(student.getAddress());
+        lblPassword.setText(student.getPassword());
     }
 
+
+    public void btnLogOutOnActon(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/studentManagement_Home.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
---
+++ src/main/java/controller/getObj.java
@@ -0,0 +1,7 @@
+package controller;
+
+import model.Student;
+
+public interface getObj {
+    public void setObj(Integer index);
+}
